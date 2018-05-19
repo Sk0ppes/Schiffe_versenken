75fce52f9325cb8ebed11d73200c288cdb1f0635
BuildTools <unconfigured@null.spigotmc.org>
2018-05-19 19:19:57 +0200

    Created (New Base class for Local and Enemy Field)

---
+++ Basefield.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchiffeFicken
+{
+    class Basefield
+    {
+        public virtual void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(0, 0);
+
+            for (int i = 0; i < 10; i++)
+                Console.Write(" " + i);
+
+            for (int i = 0; i < 10; i++)
+                Console.Write("\n" + "abcdefghij"[i] + String.Concat(Enumerable.Repeat(" ", 19)));
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+    }
+}
